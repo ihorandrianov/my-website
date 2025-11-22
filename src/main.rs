75499412cb46a887d38025cfd2b9a2e4e04f0556
Ihor Andrianov <andrianoia@gmail.com>
2025-11-23 00:16:33 +0200

    fix cv path

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use tower_http::services::{ServeDir, ServeFile};
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .nest_service("/cv/", get_service(ServeFile::new("static/cv/cv.pdf")))
+        .route_service("/cv", get_service(ServeFile::new("static/cv/cv.pdf")))
+        .route_service("/cv/", get_service(ServeFile::new("static/cv/cv.pdf")))
         .fallback_service(get_service(ServeDir::new("static")))
         .into_make_service();
 
