ee3d8010746e1e469f7a0fcccac5d048b50dcc0e
Ihor Andrianov <andrianoia@gmail.com>
2025-11-22 16:45:16 +0200

    upd

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use axum::{
-    http::StatusCode,
-    response::IntoResponse,
     routing::{get, get_service},
     Router,
 };
@@ -9,18 +7,12 @@ use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let api_router = Router::<()>::new().route("/hello", get(hello));
-
     let app = Router::new()
-        .nest("/api", api_router)
-        .fallback_service(get_service(ServeDir::new("static")))
+        .nest_service("/", get_service(ServeDir::new("static/main_page")))
+        .nest_service("/blog/", get_service(ServeDir::new("static/blog")))
         .into_make_service();
 
     let listener = TcpListener::bind("0.0.0.0:6500").await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
-}
-
-async fn hello() -> impl IntoResponse {
-    return (StatusCode::OK, "Hello!");
 }
