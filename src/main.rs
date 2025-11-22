240603e9283120af01dc4da70232635ff01fd670
Ihor Andrianov <andrianoia@gmail.com>
2025-11-22 19:29:18 +0200

    fix cv

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ use axum::{
     Router,
 };
 use tokio::net::TcpListener;
-use tower_http::services::ServeDir;
+use tower_http::services::{ServeDir, ServeFile};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .nest_service("/blog", get_service(ServeDir::new("static/blog")))
+        .nest_service("/cv", get_service(ServeFile::new("static/cv/cv.pdf")))
         .nest_service("/", get_service(ServeDir::new("static/main_page")))
         .into_make_service();
 
