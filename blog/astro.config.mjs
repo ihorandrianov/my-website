589a7a007113ed31f7763e012e298733836d1bac
Ihor Andrianov <andrianoia@gmail.com>
2025-11-22 19:22:30 +0200

    fix relative path to blogs

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://andrianov.dev',
   outDir: '../static/blog',
   base: '/blog',
+  trailingSlash: 'always',
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
