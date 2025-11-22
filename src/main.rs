b9a91ab179e6f9a4e0d93021fdb776f13287c4f1
Ihor Andrianov <andrianoia@gmail.com>
2025-11-22 23:29:43 +0200

    fix cv path

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-use axum::{
-    routing::get_service,
-    Router,
-};
+use axum::{routing::get_service, Router};
 use tokio::net::TcpListener;
 use tower_http::services::{ServeDir, ServeFile};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .nest_service("/cv", get_service(ServeFile::new("static/cv/cv.pdf")))
+        .nest_service("/cv/", get_service(ServeFile::new("static/cv/cv.pdf")))
         .fallback_service(get_service(ServeDir::new("static")))
         .into_make_service();
 
