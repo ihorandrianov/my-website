12d48143db387ded822260aa883fcff323cbbfc0
Ihor Andrianov <andrianoia@gmail.com>
2025-12-29 01:03:21 +0200

    publish blogpost

---
+++ static/_astro/Scene3DLazy.BsdMpFC-.js
@@ -0,0 +1,1 @@
+import{S as a}from"./Scene3DLazy.CnKHyS69.js";import"./index.1QzT879p.js";export{a as default};
---
+++ static/content-modules.mjs
@@ -0,0 +1,4 @@
+
+export default new Map([
+["src/content/posts/full-stack-rust-watering-system.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ffull-stack-rust-watering-system.mdx&astroContentModuleFlag=true")]]);
+		
